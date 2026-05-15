49636b37b43a905f5f2d36ebc9e3f16e14b10895
Muhammad Dayyan Tipu <dayyantipu@gmail.com>
2026-05-15 19:01:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -117,5 +117,4 @@ def chat():
 
 # Cloud servers provide a 'PORT' variable. If not, it defaults to 5000 for local testing.
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
